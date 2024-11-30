ee9dcf2c628a4a1eca8626236dce30c8529d8212
Yisus2969 <jesusangel2969@gmail.com>
2024-11-30 16:21:02 -0600

    prac 2- primer commit

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-var articulo = document.getElementById("articulo");
-var monto = document.getElementById("monto");
+function imprimirTexto() {
+  var campoTexto = document.getElementById("campoTxt");
+  var texto = campoTexto.value;
 
-var parrafoTotal = document.querySelector("p");
-var lista = document.querySelector("ul");
+  console.log(texto);
+}
+function borrarTexto() {
+  var campoTexto = document.getElementById("campoTxt");
+  campoTexto.value = "";
 
-var total = 0;
-
-function calcularTotal() {
-  var articuloValue = articulo.value;
-  var montoValue = monto.value;
-
-  total = total + parseInt(montoValue);
-
-  var elemento = document.createElement("li");
-  elemento.innerHTML = `${articuloValue}.......<strong>$${montoValue}</strong>`;
-
-  lista.appendChild(elemento);
-
-  parrafoTotal.innerHTML = `<strong>Total..........$${total}</strong>`;
+  console.log(texto);
 }
